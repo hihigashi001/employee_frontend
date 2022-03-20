37284e552c2b038e022f051aca5dc046348e16fa
hihigashi001 <hihigashi001@gmail.com>
2022-03-20 14:45:25 +0900

    api向き変更

--- src/states/APIs.ts
+++ src/states/APIs.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const apiUrl = "http://localhost:777";
+const apiUrl = "http://api3.amidakuji.net:777";
 
 export const get_user_all = async () => {
   const res = await axios.get(`${apiUrl}/users`);
